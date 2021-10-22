4f94eaf933b1926bdda08a8c714e158331ce068d
Dohyeon Ko <kodo@kakao.com>
2021-10-22 16:17:18 +0900

    2021.10.22 (Fri)

---
+++ HackerRank/8. Apple and Change.kt
@@ -0,0 +1,82 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'countApplesAndOranges' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER s
+ *  2. INTEGER t
+ *  3. INTEGER a
+ *  4. INTEGER b
+ *  5. INTEGER_ARRAY apples
+ *  6. INTEGER_ARRAY oranges
+ */
+
+fun countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: Array<Int>, oranges: Array<Int>): Unit {
+    // Write your code here
+    var applePoints = arrayListOf<Int>()
+    var orangePoints = arrayListOf<Int>()
+    var cntApple = 0
+    var cntOrange = 0
+    
+    apples.toList().map{applePoints.add(it + a)}
+    oranges.toList().map{orangePoints.add(it + b)}
+    
+    for (i in 0 until applePoints.size) {
+        if (applePoints[i] >= s && applePoints[i] <= t) {
+            cntApple += 1
+        }
+    }
+    
+    for (i in 0 until orangePoints.size) {
+        if (orangePoints[i] >= s && orangePoints[i] <= t) {
+            cntOrange += 1
+        } 
+    }
+    
+    println(cntApple)
+    println(cntOrange)
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val s = first_multiple_input[0].toInt()
+
+    val t = first_multiple_input[1].toInt()
+
+    val second_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val a = second_multiple_input[0].toInt()
+
+    val b = second_multiple_input[1].toInt()
+
+    val third_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val m = third_multiple_input[0].toInt()
+
+    val n = third_multiple_input[1].toInt()
+
+    val apples = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val oranges = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
+}
