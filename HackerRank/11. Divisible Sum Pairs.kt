52face9b65a86f9b31d191d40f131fb534d26962
Dohyeon Ko <kodo@kakao.com>
2021-10-23 10:53:13 +0900

    2021.10.23 (Sat)

---
+++ HackerRank/11. Divisible Sum Pairs.kt
@@ -0,0 +1,55 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'divisibleSumPairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY ar
+ */
+
+fun divisibleSumPairs(n: Int, k: Int, ar: Array<Int>): Int {
+    // Write your code here
+    var answer = 0
+    for (i in 0 until ar.size - 1) {
+        for (j in i + 1 until ar.size) {
+            if ((ar[i] + ar[j]) % k == 0) {
+                answer += 1
+            } 
+        }
+    }
+    return answer
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val n = first_multiple_input[0].toInt()
+
+    val k = first_multiple_input[1].toInt()
+
+    val ar = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = divisibleSumPairs(n, k, ar)
+
+    println(result)
+}
