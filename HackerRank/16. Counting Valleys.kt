0557335588de1b9900ccf47a61a8b5e7483f0aed
Dohyeon Ko <kodo@kakao.com>
2021-10-28 20:22:10 +0900

    2021.10.28 (Thr)

---
+++ HackerRank/16. Counting Valleys.kt
@@ -0,0 +1,54 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+fun countingValleys(steps: Int, path: String): Int {
+    // Write your code here
+    var height = 0
+    var cnt = 0
+    for (e in path.iterator()) {
+        if (e == 'U') {
+            height += 1
+            if (height == 0) {
+                cnt += 1
+            }
+        } else {
+            height -= 1
+        }
+    }
+    return cnt
+}
+
+fun main(args: Array<String>) {
+    val steps = readLine()!!.trim().toInt()
+
+    val path = readLine()!!
+
+    val result = countingValleys(steps, path)
+
+    println(result)
+}
