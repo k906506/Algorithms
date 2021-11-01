218ea335ff62c05e46a3db6a20fe53100701293f
Dohyeon Ko <kodo@kakao.com>
2021-11-02 00:45:33 +0900

    2021.11.02 (Tue)

---
+++ HackerRank/21. Designer PDF Viewer.kt
@@ -0,0 +1,49 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'designerPdfViewer' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY h
+ *  2. STRING word
+ */
+
+fun designerPdfViewer(h: Array<Int>, word: String): Int {
+    // Write your code here
+    var array = arrayListOf<Int>()
+    
+    for (e in word) {
+        array.add(h[e.toInt() - 97])
+    }
+
+    return array.max()!! * word.length
+}
+
+fun main(args: Array<String>) {
+
+    val h = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val word = readLine()!!
+
+    val result = designerPdfViewer(h, word)
+
+    println(result)
+}
