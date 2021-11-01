f3ac39539044ebf8fa0d74281272a158571cf05b
Dohyeon Ko <kodo@kakao.com>
2021-11-02 00:37:23 +0900

    2021.11.02 (Tue)

---
+++ HackerRank/20. The Hurdle Race.kt
@@ -0,0 +1,58 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'hurdleRace' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY height
+ */
+
+fun hurdleRace(k: Int, height: Array<Int>): Int {
+    // Write your code here
+    var max = k
+    
+    for (i in height.indices) {
+        if (height[i] > max) {
+            max = height[i]
+        }
+    }
+    
+    if (max == k) {
+        return 0
+    } else {
+        return max - k
+    }
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val n = first_multiple_input[0].toInt()
+
+    val k = first_multiple_input[1].toInt()
+
+    val height = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = hurdleRace(k, height)
+
+    println(result)
+}
