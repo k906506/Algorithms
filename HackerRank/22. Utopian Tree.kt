1402b46be36a940eb45383877e40ab253ae70d13
Dohyeon Ko <kodo@kakao.com>
2021-11-02 00:54:38 +0900

    2021.11.02 (Tue)

---
+++ HackerRank/22. Utopian Tree.kt
@@ -0,0 +1,51 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'utopianTree' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+fun utopianTree(n: Int): Int {
+    // Write your code here
+    var length = 1
+    
+    for (i in 1..n) {
+        if (i % 2 == 0) {
+            length += 1
+        } else {
+            length *= 2
+        }
+    }
+    return length
+}
+
+fun main(args: Array<String>) {
+    val t = readLine()!!.trim().toInt()
+
+    for (tItr in 1..t) {
+        val n = readLine()!!.trim().toInt()
+
+        val result = utopianTree(n)
+
+        println(result)
+    }
+}
