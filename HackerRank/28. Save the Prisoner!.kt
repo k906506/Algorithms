07127ca7719d4df3e5b87d6fe844366bdcfc5d26
Dohyeon Ko <kodo@kakao.com>
2021-12-11 21:22:58 +0900

    2021.12.11 (Sat)

---
+++ HackerRank/28. Save the Prisoner!.kt
@@ -0,0 +1,59 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'saveThePrisoner' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ *  3. INTEGER s
+ */
+
+fun saveThePrisoner(n: Int, m: Int, s: Int): Int {
+    // Write your code here
+    var temp = m % n - 1
+    if (temp < 0) {
+        temp = temp + n
+    }
+    var answer = s + temp 
+    if (answer > n) {
+        answer %= n
+    }
+    return answer
+}
+
+fun main(args: Array<String>) {
+    val t = readLine()!!.trim().toInt()
+
+    for (tItr in 1..t) {
+        val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+        val n = first_multiple_input[0].toInt()
+
+        val m = first_multiple_input[1].toInt()
+
+        val s = first_multiple_input[2].toInt()
+
+        val result = saveThePrisoner(n, m, s)
+
+        println(result)
+    }
+}
