ad6cf94c9a3f18e4ec4b91c759e12284b2b45383
Dohyeon Ko <kodo@kakao.com>
2021-12-20 23:51:45 +0900

    2021.12.20 (Mon)

---
+++ HackerRank/31. Jumping on the Clouds.kt
@@ -0,0 +1,40 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'jumpingOnClouds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY c as parameter.
+ */
+
+fun jumpingOnClouds(c: Array<Int>): Int {
+    // Write your code here
+
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val c = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = jumpingOnClouds(c)
+
+    println(result)
+}
