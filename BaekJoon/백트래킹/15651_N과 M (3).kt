93a8d47f2ac49d53f4c58b64333b8f13c0fa8f7d
Dohyeon Ko <kodo@kakao.com>
2022-03-28 15:14:49 +0900

    2021.03.28 (Mon)

---
+++ BaekJoon/백트래킹/15651_N과 M (3).kt
@@ -0,0 +1,34 @@
+import java.util.*
+
+lateinit var visit: Array<Int>
+lateinit var input: Array<Int>
+var n: Int = 0
+var m: Int = 0
+var stringBuilder = StringBuilder()
+
+fun main() = with(Scanner(System.`in`)) {
+    n = nextInt()
+    m = nextInt()
+
+    visit = Array(n + 1) { 0 }
+    input = Array(n + 1) { 0 }
+
+    dfs(0)
+    println(stringBuilder)
+}
+
+fun dfs(depth: Int) {
+    if (depth == m) {
+        for (i in 0 until m) {
+            stringBuilder.append(input[i]).append(" ")
+        }
+        stringBuilder.append("\n")
+        return
+    }
+
+    for (i in 0 until n) {
+        input[depth] = i + 1
+        dfs(depth + 1)
+    }
+    return
+}
