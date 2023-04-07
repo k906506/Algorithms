9e4d4003bd5333d73faf18ba05d993cae224653e
k906506 <k906506@naver.com>
2023-04-07 13:36:35 +0900

    2023.04.07

---
+++ SQL/LEVEL 4/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -0,0 +1,23 @@
+SELECT
+    MEMBER_NAME,
+    REVIEW_TEXT,
+    DATE_FORMAT(REVIEW_DATE, "%Y-%m-%d") AS REVIEW_DATE
+FROM
+    MEMBER_PROFILE AS A
+    INNER JOIN REST_REVIEW AS B ON A.MEMBER_ID = B.MEMBER_ID
+WHERE
+    A.MEMBER_ID = (
+        SELECT
+            MEMBER_ID
+        FROM
+            REST_REVIEW
+        GROUP BY
+            MEMBER_ID
+        ORDER BY
+            COUNT(MEMBER_ID) DESC
+        LIMIT
+            1
+    )
+ORDER BY
+    REVIEW_DATE,
+    REVIEW_TEXTß
